e2a3ca1203a9045793a1c7dfdc3ce198b16ee3a1
Nico Alsemgeest <nalsemgeest@conclusion.nl>
2020-09-07 12:20:06 +0200

    Derde

---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+// src/components/Title.tsx
+import React from "react";
+
+type Props = {
+  content: string;
+};
+
+export default function Title({ content }: Props) {
+  return <h1>{content}</h1>;
+}
